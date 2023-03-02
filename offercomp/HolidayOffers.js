71c9a3962147d7f0d398a3808c2842beabc54857
slydesigns <sylvestorsielei@gmail.com>
2023-03-02 20:00:18 +0300

    Const

---
+++ offercomp/HolidayOffers.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useQuery, useMutation, gql } from '@apollo/client';
+import OfferTile from './OfferTile';
+
+const GET_OFFERS = gql`
+  query GetOffers {
+    offers(limit: 10, sort: { by: DATE_ADDED, order: DESC }) {
+      id
+      name
+      visitedCount
+      price: value(currency: USD)
+      imageUrl
+      description
+      dateAdded
+    }
+  }
+`;
